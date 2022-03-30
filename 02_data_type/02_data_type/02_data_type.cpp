607d53dfe3ca5c8a5ea6c4ec17c46cecdd8c9b44
MFKY <yky2367@hotmail.com>
2022-03-31 01:01:33 +0800

    Day 2 file_02 data_type completed

--- 02_data_type/02_data_type/02_data_type.cpp
+++ 02_data_type/02_data_type/02_data_type.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
 
@@ -20,10 +21,58 @@ int main()
 	cout << c << endl;
 	float d = 3e-2;//3*10^-2
 	cout << d << endl;
- 	system("pause");
+ 	
 
-	//字符型
+	//字符型 char 变量名 = 'x'
+	char name = 'b';//单引号里面只能写一个字符
+	char sex = 'B';
+	cout <<  name << endl;
+	cout << "char的内存大小为" << sizeof(char) << endl;
+	
+	//使用字符型的常见错误
+	//1、创建字符型变量需要用单引号
+	//2、字符型变量，单引号内只能放一个字符
 
+	//查看字符型变量的ASCII编码
+	cout << (int)name << endl;  //将name强制转换成int整型，输出他代表字符的ASCII码
+	cout << (int)sex << endl;
+	// a -> 97 A ->65  b->98 B->66
+
+
+
+	//转义字符  \n 换行符 \t 制表符 \\反斜杠
+	char one = 'a';
+	char two = 'b';
+	cout << one << "\n" << two << endl;
+	//水平制表符 有助于输出时对其 *占据8个位置
+	cout << "kaiyuan\t19950927" << endl;
+	cout << "ky\t19950927" << endl;
+	cout << "yky\t19950927" << endl;
+
+
+	//C风格字符串型   char 变量名[] = "xxxxxxxx"  中括号和双引号
+	//C++风格字符串型   string 变量名 = "xxxxxx"  *要包含头文件 #include<string>
+
+	char str1[] = "hello world";
+	cout << str1 << endl;
+
+	string str2 = "Hello world";
+	cout << str2 << endl;
+
+	//bool  布尔类型
+	//占用一个字节大小  true 1  false 0
+	bool flag = true;
+	cout << flag << endl;
+	cout << sizeof(flag) << endl;
+
+	//数据的输入 从键盘获取 cin >> 变量
+	int e = 0;
+	cout << "请给整型e赋值" << endl;
+	cin >> e;
+	cout << e << endl;
+
+	//批量注释 CTRL + K + C
+	system("pause");
 
 	return 0;
 
